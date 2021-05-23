ead66eaed148705159f117b5c6b5e613f9f4fceb
Zack Umar <zackumar@gmail.com>
2021-05-23 03:18:04 +0000

    Added twitter poster

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world')
+import cv, { Rect } from 'opencv4nodejs'
+import * as nw from './newspaper'
+import * as crop from './crop'
+import * as post from './post'
+;(async () => {
+    // let paper = await nw.downloadJP2()
+    // cv.imwrite('./image.jpg', paper.image)
+    // let image = cv.imread('./image.jpg')
+    // let bbs = crop.findBoxes(image)
+    // console.log(bbs)
+    // const region = crop.crop(image, bbs[0])
+    // console.log(region)
+    // cv.imwrite('./cropped.jpg', region)
+    let credentials = {
+        consumer_key: 'SjuqcUEDFBpbfbETpdrBKp5N9',
+        consumer_secret: '7XW4FGXsV34VGJVsSIJ5WHWAOD0apH6WXKNVaOs5QyYZ6qJgrS',
+        access_token_key: '914188211718586368-a7JKgkuEqEDWCMJhinPvABrNlr0w0n1',
+        access_token_secret: 'jQUOI6Ch3cIRFMrubqDujMzmCv9otCzKwJdPpTflKT239',
+    }
+    post.postTwitter(credentials, './cropped.jpg', 'test')
+})()
---
+++ src/post.ts
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import { IgApiClient } from 'instagram-private-api'
+import Twitter from 'twitter'
+
+export async function postInstagram(credentials: any, filename: string, caption: string) {
+    let client = new IgApiClient()
+    client.state.generateDevice(credentials['username'])
+    await client.account.login(credentials['username'], credentials['password'])
+
+    let publishResults = await client.publish.photo({
+        file: fs.readFileSync(filename),
+        caption: caption,
+    })
+
+    return publishResults.status === 'ok'
+}
+
+export async function postTwitter(credentials: any, filename: string, caption: string) {
+    let client = new Twitter(credentials)
+    client.post('media/upload', { media: fs.readFileSync(filename) }, (err, media, response) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        var status = {
+            status: caption,
+            media_ids: media.media_id_string,
+        }
+
+        client.post('statuses/update', status, (err, tweet, response) => {
+            if (err) console.error(err)
+        })
+    })
+}
